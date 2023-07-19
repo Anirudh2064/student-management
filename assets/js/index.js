53c4a4a9a60994797837bd2af14faeaf51de4374
Anirudh2064 <anirudhpeddagoni@gmail.com>
2023-07-19 08:38:55 +0530

    first commit

---
+++ assets/js/index.js
@@ -0,0 +1,53 @@
+
+
+$("#add_user").submit(function(event){
+    alert("Data Inserted Succesfully");    
+})
+
+$("#update_user").submit(function(event){
+    event.preventDefault();
+
+    var unindexed_array = $(this).serializeArray();
+    var data = {}
+
+    $.map(unindexed_array, function(n, i){
+        data[n['name']] = n['value']
+    })
+
+    console.log(data.port);
+
+    var request = {
+        "url" : `http://localhost:${data.port}/api/users/${data.id}`,
+        "method" : "PUT",
+        "data" : data
+    }
+
+    $.ajax(request).done(function(response){
+        alert("Data Updated Successfully!");
+    })
+
+})
+
+if(window.location.pathname=="/"){
+    $ondelete = $(".table tbody td a.delete");
+    $ondelete.click(function(){
+        var id = $(this).attr("data-id")
+        var port = $(this).attr("name")
+
+        console.log(port);
+
+
+        var request = {
+            "url" : `http://localhost:${port}/api/users/${id}`,
+            "method" : "DELETE"
+        }
+
+        if(confirm("Do you really want to delete this record?")){
+            $.ajax(request).done(function(response){
+                alert("Data Deleted Successfully!");
+                location.reload()
+            })            
+        }
+
+    })
+}
